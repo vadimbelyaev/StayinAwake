0261bcdc8d870ec31fde11892caddcea3c7148f0
Vadim Belyaev <vadimbelyaev@gmail.com>
2020-01-23 15:52:21 +0300

    Initial Commit

---
+++ StayinAwake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StayinAwake
+//
+//  Created by Vadim Belyaev on 23.01.2020.
+//  Copyright © 2020 Vadim Belyaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

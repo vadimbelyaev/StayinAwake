cbb13aca40b9c41aadc4e6fc264a03dcd2913995
Vadim Belyaev <vadimbelyaev@gmail.com>
2020-01-26 03:00:17 +0300

    Disabled idle timer in order to prevent the device from going to sleep while the app is running

--- StayinAwake/ViewController.swift
+++ StayinAwake/ViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        UIApplication.shared.isIdleTimerDisabled = true
     }
 
-
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
 }
-
